22ba07f434837b948b9530fb71d30fa62de574f7
ZhouLu <lz354000575@gmail.com>
2023-09-16 21:15:28 +0900

    update 007

--- Py_selfLearing/004.py
+++ Py_selfLearing/004.py
@@ -1,4 +1,4 @@
-# enumerate
+#004 enumerate
 '''
 enumerate 是一个内置的 Python 函数，用于在遍历一个可迭代对象（如列表、元组、字符串等）时获取元素及其索引。
 enumerate 函数返回一个迭代器，该迭代器生成由索引和元素组成的元组
---
+++ Py_selfLearing/007.py
@@ -0,0 +1,32 @@
+#007 列表推导式&生成器表达式
+'''
+列表推导式（List Comprehensions）
+[expression for element in iterable if condition]
+
+列表推导式 -> 列表 -> !!!方括号!!!
+
+生成器表达式（Generator Expressions）
+(expression for element in iterable if condition)
+
+生成器表达式 -> 对象 -> !!!圆括号!!!
+生成器是惰性求值（lazy evaluation），这意味着它只在需要时计算下一个值。
+
+区别：
+1. 内存使用：生成器表达式更内存高效，因为它们不一次性生成所有值。这在处理大数据集时特别有用。
+2. 用途：列表推导式主要用于生成新的列表。生成器表达式则更多地用于迭代操作，不需要一次性生成所有数据。
+3. 语法：两者的语法非常相似，主要区别在于使用的括号类型（方括号用于列表推导式，圆括号用于生成器表达式）。
+'''
+
+squares1 = [x*x for x in range(10)]
+print(squares1)
+print(min(squares1))
+
+squares2 = (x*x for x in range(10))
+print(squares2)
+print(min(squares2))
+
+'''
+惰性求值（Lazy Evaluation）是一种计算策略，用于推迟表达式的求值，直到实际需要这个值为止。
+这种方式可以提高程序的性能，并允许创建无限的数据结构。
+在惰性求值模型中，当你创建一个表达式或数据结构（例如生成器）时，计算不会立即执行。相反，每次需要一个新的值时，该值才会被计算。
+'''
